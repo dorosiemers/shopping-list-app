2307d568c5b5dfeb4f59c28b4b2673d2b57cb5b1
Doro Siemers <dorotheasiemers@gmail.com>
2022-10-09 13:18:14 +0200

    Saved new Headline

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div className="main">
-      <h1>React Search</h1>
+      <h1>Shopping List App</h1>
       <div className="search">
         <TextField
           id="outlined-basic"
